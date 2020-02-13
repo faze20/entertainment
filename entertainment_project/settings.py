8d87fecdd1f61343d813770e1a088789643e6423
faze20 <42577269+faze20@users.noreply.github.com>
2020-02-13 20:48:03 +0000

    docs

--- entertainment_project/settings.py
+++ entertainment_project/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     #3rd party apps
     'crispy_forms',
     
-    #local Apps
+    #local Applications 
     'movie_center.apps.MovieCenterConfig',
     'user.apps.UserConfig',
 ]
